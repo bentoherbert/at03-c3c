50fcc82a3193091b17aaa991e3f4af9e61c0ea9a
bentoherbert <100323996+bentoherbert@users.noreply.github.com>
2022-08-27 11:53:33 -0300

    Commit da questão 4
    Atualização do index.html e questao4.js

--- Questão 4/questao4.js
+++ Questão 4/questao4.js
@@ -1,1 +1,28 @@
+window.onload = function onloadquestao4() {
+  let questao4 = document.getElementById("questao4");
+  questao4.innerHTML =
+    "<label>Digite um número: </label> </br>" +
+    "<input id='num' type='text' placeholder='Digite o número' onKeyPress='return SomenteNumero(event)'>" +
+    "<input type='button' value='Clique' onClick='verificaArray()'>";
+};
 
+function verificaArray() {
+  // Variavél do limite que foi preenchido no input.
+  n = Number(document.getElementById("num").value);
+  // Array que irá receber os acréscimos.
+  let arrayNum = [];
+  // Looping
+  for (let i = 1; i <= n; i++) {
+    // Looping para identificar os números
+    if (i % 5 == 0 && i % 9 == 0) {
+      arrayNum.push("LuidyMoura");
+    } else if (i % 9 == 0 && i % 5 != 0) {
+      arrayNum.push("Moura");
+    } else if (i % 5 == 0 && i % 9 != 0) {
+      arrayNum.push("Luidy");
+    } else {
+      arrayNum.push(i);
+    }
+  }
+  document.write(arrayNum);
+}
